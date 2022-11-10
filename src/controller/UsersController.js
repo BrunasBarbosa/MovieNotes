3df66b8d3c491ab2a355088b4614ce38338daa8e
Bruna Barbosa <brunas_barbosa@hotmail.com>
2022-11-09 22:10:48 -0300

    handle errors

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -1,10 +1,17 @@
+const { hash, compare } = require('bcryptjs');
+
+const AppError = require('../utils/AppError');
+
 // const sqliteConnection = require('../database/sqlite');
 
 class UsersController {
   async creat(request, response) {
     const { name, email, password } = request.body;
-    // const database = await sqliteConnection;
+    // const database = await sqliteConnection();
+    
     response.send({ name, email, password });
+
+    const hashedPassword = await hash(password, 8);
   }
 }
 
