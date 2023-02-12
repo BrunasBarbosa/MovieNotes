0c2d1ce68edd15f473c03f284e43f9f84d556b30
Bruna Barbosa <brunas_barbosa@hotmail.com>
2023-02-12 13:40:59 -0300

    check password

--- src/controller/SessionsController.js
+++ src/controller/SessionsController.js
@@ -1,6 +1,6 @@
 const AppError = require('../utils/AppError');
 const knex = require('../database/knex');
-
+const { compare } = require('bcryptjs');
 class SessionsController {
   async create(request, response) {
     const { email, password } = request.body;
@@ -9,6 +9,12 @@ class SessionsController {
 
     if (!user) {
       throw new AppError("E-mail e/ou senha incorreto(a).", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("E-mail e/ou senha incorreto(a)", 401);
     }
     return response.json(user);
   }
