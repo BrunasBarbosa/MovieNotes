cb7dd382ea7688549931edbd6bda412a8eb2aa5c
Bruna Barbosa <brunas_barbosa@hotmail.com>
2023-02-12 16:58:26 -0300

    use token at notes route

--- src/controller/NotesController.js
+++ src/controller/NotesController.js
@@ -3,7 +3,7 @@ class NotesController {
   async create(request, response) {
 
     const { title, description, rating, tags } = request.body;
-    const { user_id } = request.params;
+    const user_id = request.user.id;
 
     const note_id = await knex('movie_notes').insert({
       title,
@@ -46,7 +46,9 @@ class NotesController {
   }
 
   async index(request, response) {
-    const { user_id, title, tags } = request.query;
+    const { title, tags } = request.query;
+
+    const user_id = request.user.id;
 
     let notes;
 
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 
 const NotesController = require('../controller/NotesController');
 
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
+
 const notesRoutes = Router();
 
 const notesController = new NotesController();
-
-notesRoutes.post('/:user_id', notesController.create);
+notesRoutes.use(ensureAuthenticated)
+notesRoutes.post('/', notesController.create);
 notesRoutes.get('/:id', notesController.show);
 notesRoutes.delete('/:id', notesController.delete);
 notesRoutes.get('/', notesController.index);
