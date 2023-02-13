118e07fcd72da428ddab4b8852db92268a545773
Bruna Barbosa <brunas_barbosa@hotmail.com>
2023-02-12 22:27:27 -0300

    handle avatar file

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const path = require('path');
 
 const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp');
-const UPLOAD_FOLDER = path.resolve(__dirname, 'uploads');
+const UPLOAD_FOLDER = path.resolve(TMP_FOLDER, 'uploads');
 
 const MULTER = {
   storage: multer.diskStorage({
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,14 +1,22 @@
 const { Router } = require('express');
 
+const uploadConfig = require('../configs/upload');
+const multer = require('multer');
+
 const UsersController = require('../controller/UsersController');
 
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
 
 usersRoutes.post('/', usersController.creat);
 usersRoutes.put('/', ensureAuthenticated, usersController.update);
+usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), (req, res) => {
+  console.log(req.file.filename);
+  res.json();
+});
 
 module.exports = usersRoutes;
