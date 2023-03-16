7af7c4e958e9241883cfbb34c43cde9136515b56
Bruna Barbosa <brunas_barbosa@hotmail.com>
2023-03-16 19:30:22 -0300

    l

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -7,7 +7,7 @@ const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 const notesRoutes = Router();
 
 const notesController = new NotesController();
-notesRoutes.use(ensureAuthenticated)
+notesRoutes.use(ensureAuthenticated);
 notesRoutes.post('/', notesController.create);
 notesRoutes.get('/:id', notesController.show);
 notesRoutes.delete('/:id', notesController.delete);
--- src/server.js
+++ src/server.js
@@ -8,7 +8,11 @@ const express = require('express');
 
 const routes = require('./routes');
 
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
