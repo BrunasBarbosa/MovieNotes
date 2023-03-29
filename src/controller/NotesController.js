00baa1904c883f2f621e0159225a5a099237dbc5
Bruna Barbosa <brunas_barbosa@hotmail.com>
2023-03-28 21:23:39 -0300

    create update note function

--- src/controller/NotesController.js
+++ src/controller/NotesController.js
@@ -2,7 +2,6 @@ const AppError = require('../utils/AppError');
 const knex = require('../database/knex');
 class NotesController {
   async create(request, response) {
-
     const { title, description, rating, tags } = request.body;
     const user_id = request.user.id;
 
