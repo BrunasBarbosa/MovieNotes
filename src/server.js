5a5961b5a00ec241c02d7e01952804b1901a615d
Bruna Barbosa <brunas_barbosa@hotmail.com>
2023-04-18 15:46:43 -0300

    create environment variables

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('express-async-errors');
+require('dotenv/config');
 
 const uploadConfig = require('./configs/upload');
 
@@ -36,6 +37,6 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
