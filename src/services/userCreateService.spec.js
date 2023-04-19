4e4490de050f41ec9abfffb4a2624b3533f9a22b
Bruna Barbosa <brunas_barbosa@hotmail.com>
2023-04-19 17:10:20 -0300

    test email already existis

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,17 +1,41 @@
 const UserCreateServices = require('./UserCreateService');
-const UserReposytoryInMemory = require('../repositories/UserReposytoryInMemory')
+const UserReposytoryInMemory = require('../repositories/UserReposytoryInMemory');
+const AppError = require('../utils/AppError');
 
-it('user should be create', async () => {
-  const userReposytoryInMemory = new UserReposytoryInMemory();
-  const userCreateServices = new UserCreateServices(userReposytoryInMemory);
-  
-  const user = {
-    name: 'Test',
-    email: 'user@test.com',
-    password: '123'
-  };
-  
-  const userCreated = await userCreateServices.execute(user);
+describe('UserCreateService', () => {
+  it('user should be create', async () => {
+    const userReposytory = new UserReposytoryInMemory();
+    const userCreateServices = new UserCreateServices(userReposytory);
 
-  expect(userCreated).toHaveProperty('id');
+    const user = {
+      name: 'Test',
+      email: 'user@test.com',
+      password: '123'
+    };
+
+    const userCreated = await userCreateServices.execute(user);
+
+    expect(userCreated).toHaveProperty('id');
+  });
+
+  it('user should not be create if email already exists', async () => {
+    const user1 = {
+      name: 'User1',
+      email: 'user@test.com',
+      password: '123'
+    };
+
+    const user2 = {
+      name: 'User2',
+      email: 'user@test.com',
+      password: '456'
+    };
+
+    const userReposytory = new UserReposytoryInMemory();
+    const userCreateServices = new UserCreateServices(userReposytory);
+
+    await userCreateServices.execute(user1);
+
+    await expect(userCreateServices.execute(user2)).rejects.toEqual(new AppError('Este e-mail já está em uso.'));
+  });
 });
